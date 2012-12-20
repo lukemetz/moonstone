2b7ddea102b8b14879cb548a76fd2acc935d3ddf
Luke Metz <luke.metz@students.olin.edu>
2012-12-20 16:53:24 -0500

    Generic Premake4 added with lua support.

---
+++ premake4.lua
@@ -0,0 +1,18 @@
+-- A solution contains projects, and defines the available configurations
+solution "moonstone"
+  configurations { "Debug", "Release" }
+  
+  project "main"
+    kind "ConsoleApp"
+    language "C++"
+    files {"*.hpp", "*.cpp"}
+    includedirs {".", "x86_64-linux-gnu/lua5.2"}
+    links{'lua5.2'}
+    buildoptions {"--std=c++11" } ---Wall -Wextra
+    configuration "Debug"
+      defines { "DEBUG" }
+      flags { "Symbols" }
+
+    configuration "Release"
+      defines { "NDEBUG" }
+      flags { "Optimize" }
