f74332e9fb7933fd91266f4e992e31a82eb7c516
= <luke.metz@students.olin.edu>
2013-01-15 23:06:54 +0000

    Don't let camera>near_clip be zero
    This is a quick fix to a a bug where the update from lua function is not
    working properly and only setting near_clip to zero.

--- src/components/Camera.cpp
+++ src/components/Camera.cpp
@@ -8,8 +8,16 @@ int Camera::get_lua_ref(lua_State * L)
 
 void Camera::update_from_lua(lua_State * L)
 {
+  //TODO This is not actually being set. It is always zero on refreshes.
   lua_getfield(L, -1, "near_clip");
+
+  //Don't allow to set to zero
   near_clip = lua_tonumber(L, -1);
+
+  if (near_clip == 0) {
+    printf("near clip cannot be zero \n");
+    near_clip = .1;
+  }
   lua_pop(L, 1);
 }
 
