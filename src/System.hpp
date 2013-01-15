1444807e4e60380e1467a79f4c09484972336f4d
= <luke.metz@students.olin.edu>
2013-01-15 23:55:34 +0000

    test fix for issue 7

--- src/System.hpp
+++ src/System.hpp
@@ -7,7 +7,7 @@ struct System
 {
   virtual void init() {};
   virtual void update(float dt) {};
-  virtual std::string get_name() {return "Generic System";};
+  virtual std::string get_name() {return std::string("Generic System");};
   virtual void reload() {};
   virtual void remove() {};
   void set_manager(Manager * manag)
