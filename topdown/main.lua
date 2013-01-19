074606eb903da6be5a319a35db97cac4cf12ea39
= <luke.metz@students.olin.edu>
2013-01-19 18:30:22 +0000

    Update topdown game test scene

--- topdown/main.lua
+++ topdown/main.lua
@@ -8,7 +8,6 @@ systems = {
   "Bullet_System",
   }
 
-
 -- helper functions that should be moved
 entities = {}
 function add_entity(entity)
@@ -26,46 +25,73 @@ function add_entities(entity_array)
 end
 
 --add camera
-add_entity{Camera = {near_clip = .1}}
+camera_entity = {
+  Camera = {near_clip = .1},
+  Transform = {
+    pos = Vec3f(0,50,0),
+    -- The quaternion for looking down
+    rot = Quat(-math.sqrt(2)/2, 0, 0, math.sqrt(2)/2)
+  },
+  camera_follower = {following = 0, y_offset = 50},
+}
+
+add_entity(camera_entity)
 
 local box_width = 4
 local box_vec = Vec3f(box_width, box_width, box_width)
+
 --player
-add_entity{
+player_id = add_entity{
   Transform = {pos = Vec3f(0,10, 0)},
   Input = {},
   Collider = {type = "box", side = box_vec},
   Rigid_Body = {mass = 3,
     friction=.1, linear_damping = .1,
-    angular_damping = 1},
+    torque = Vec3f(0, 0, 0),
+    angular_damping = .3,
+    angular_factor = Vec3f(0, 1, 0),
+  },
   player_movement = {
-    force_mult = 5000, 
+    force_mult = 5000,
     force_max = 500,
     max_vel = -20,
     vel_reduction = 50,
     jump_vel = 10,
-    has_jumped = false,
+    has_jumped = false
   },
   Mesh = { filename = "Player.mesh" },
 }
 
-for i=1,10 do
-  add_entity{
-    Transform = {pos = Vec3f(0, 10+i*4, 0)},
-    Collider = {type = "box", side = box_vec},
-    Rigid_Body = {mass = 1, friction = .5},
-    Mesh = { filename = "Enemy.mesh" },
-    Light = { },
-  }
+camera_entity.camera_follower.following = player_id
+
+for x=-5,5 do
+  for y=-5,5 do
+    add_entity{
+      Transform = {pos = Vec3f(x*10, 20, y*10)},
+      Collider = {type = "box", side = box_vec},
+      Rigid_Body = {mass = 10, friction = .5},
+      Mesh = { filename = "Enemy.mesh" },
+    }
+  end
+end
+
+for x=-1,1 do
+  for y=-1,1 do
+    add_entity{
+      Transform = {pos = Vec3f(x*5, 4, y*5) },
+      Light = {}
+   }
+  end
 end
 
 --ground
 add_entity{
   Transform = {pos = Vec3f(0,-10, 0)},
-  Collider = {type = "box", side = Vec3f(100, 4, 100)},
+  Collider = {type = "box", side = Vec3f(200, 4, 200)},
   Rigid_Body = {mass = 0, friction = .65}
 }
 
+--Update once per frame
 add_entity {
   single = {paused = false, keydown = false},
   Input = {}
